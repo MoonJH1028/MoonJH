60b644f311afb5b1f46d2ccc4c02dc5db09d8017
MoonJH1028 <107673624+MoonJH1028@users.noreply.github.com>
2023-10-13 16:29:27 +0900

    Add files via upload

--- 새 폴더/main.js
+++ 새 폴더/main.js
@@ -1,10 +1,21 @@
-let year = prompt('올해가 몇년일까요?')
+const comPic = document.querySelector(".comPic");
+const myPicR = document.querySelector(".myPicR");
+const myPicS = document.querySelector(".myPicS");
+const myPicP = document.querySelector(".myPicP");
 
-if (year == 2023) {
-    alert( "ㅇㅇ");
-    alert("머리좋노");
-} else if (year > 2020) {
-    alert ("정신차려"); 
-} else {
-    alert("ㅄ");
+const RSP = ["묵","찌","빠"];
+
+function randomPic() {
+    const randomNum = Math.floor(Math.random() * 3);
+    const randomRSP = RSP[randomNum];
+    return randomRSP;
 }
+
+function mainFunction() {
+    const randomRSP = randomPic();
+    comPic.innerText = randomRSP;
+}
+
+myPicR.addEventListener("click", mainFunction)
+myPicS.addEventListener("click", mainFunction)
+myPicP.addEventListener("click", mainFunction)
